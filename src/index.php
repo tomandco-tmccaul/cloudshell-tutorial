c8b3914e6a05019edfeec78cc16eea2101d83261
tomandco <tom@tomandco.co.uk>
2019-05-24 21:44:48 +0100

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,7 +1,7 @@
 <?php
 
 echo "Hello, World from Kubernetes! <br>";
-echo "Hello Jenkins X! <br>";
+echo "Hello Jenkins X V2! <br>";
 echo '<img border="0" src="https://jenkins-x.io/images/logo.png" width="300" height="300" />';
 
 ?>
